311921a718bb8ee98d766b89c02a726bf58ec8ef
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 21:04:40 +0000

    Adding tarfile member sanitization to extractall()

--- tools/addon_updater.py
+++ tools/addon_updater.py
@@ -122,7 +122,26 @@ def update_addon_repo():
     response = requests.get(addon_repo_url, stream=True)
     try:
         with tarfile.open(fileobj=response.raw, mode="r|gz") as tarball:
-            tarball.extractall(addon_repo_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tarball, addon_repo_dir)
     except tarfile.ReadError as e:
         print(
             "Error downloading repository tarball {}, did you specify an existing branch from {}?".format(
