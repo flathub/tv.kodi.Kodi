e9e8a1af169e6e74a859bd9309f966ac5cde2479
Kolja Lampe <razzeee@gmail.com>
2025-04-28 00:27:31 +0200

    Update kodi and binary addons

--- tools/addon_updater.py
+++ tools/addon_updater.py
@@ -98,7 +98,7 @@ def set_build_type(a_data):
             a_data["config-opts"].append("-DCMAKE_BUILD_TYPE=Release")
         a_data["build-options"]["no-debuginfo"] = True
         a_data["build-options"]["cflags"] = "-g0"
-        a_data["build-options"]["cxxflags"] = "-g0"
+        a_data["build-options"]["cxxflags"] = "-g0" if a_data["name"] != "pvr.iptvsimple" else "-g0 -Wp,-U_GLIBCXX_ASSERTIONS"
     else:
         if "-DCMAKE_BUILD_TYPE=Release" in a_data["config-opts"]:
             a_data["config-opts"].remove("-DCMAKE_BUILD_TYPE=Release")
